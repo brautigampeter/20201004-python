0161083a52f3c49027814ae1f3e5ed09f4327c5e
brautigampeter <brautigampeter222@gmail.com>
2022-10-04 13:32:30 +0200

    feladat2.2

--- burkolo.py
+++ burkolo.py
@@ -1,4 +1,11 @@
-szelesseg=int(input("Szélesség (m): "))
-magassag=int(input("Magasság (m): "))
+import math
 
-print()
+szelesseg=float(input("Szélesség (m): "))
+magassag=float(input("Magasság (m): "))
+
+T=(szelesseg*100)*(magassag*100)
+
+Tcsempe=20*20
+
+print(math.ceil((T/Tcsempe)*1.1),"db csempe kell")
+
